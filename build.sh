4211c828be6f77f96d97176dd5b88938c0a0229a
Aleksandr Litvinenko <alekslitvinenk@gmail.com>
2019-08-30 22:23:12 +0300

    WIP3

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ rm -R target
 
 sbt ';build'
 
-docker build -t alekslitvinenk/doppler:latest .
-docker push alekslitvinenk/doppler:latest
+docker build -t alekslitvinenk/doppler:latest . --no-cache
+#docker push alekslitvinenk/doppler:latest
--- src/main/scala/com/alekslitvinenk/doppler/Main.scala
+++ src/main/scala/com/alekslitvinenk/doppler/Main.scala
@@ -1,14 +1,13 @@
 package com.alekslitvinenk.doppler
 
-import java.io.InputStream
-import java.security.{KeyStore, SecureRandom}
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.{ConnectionContext, Http, HttpsConnectionContext}
 import akka.stream.ActorMaterializer
 import com.alekslitvinenk.logshingles.dsl.ShinglesDirectives._
-import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
+import com.typesafe.sslconfig.akka.AkkaSSLConfig
+import com.typesafe.sslconfig.akka.util.AkkaLoggerFactory
+import com.typesafe.sslconfig.ssl.ConfigSSLContextBuilder
 
 import scala.concurrent.ExecutionContext
 import scala.util.Try
@@ -18,26 +17,6 @@ object Main extends App {
   implicit val system: ActorSystem = ActorSystem("my-system")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContext = system.dispatcher
-
-  // Manual HTTPS configuration
-
-  val password: Array[Char] = "".toCharArray // do not store passwords in code, read them from somewhere safe!
-
-  val ks: KeyStore = KeyStore.getInstance("PKCS12")
-  val keystore: InputStream = getClass.getClassLoader.getResourceAsStream("certificates/alekslitvinenk.com/server.p12")
-
-  require(keystore != null, "Keystore required!")
-  ks.load(keystore, password)
-
-  val keyManagerFactory: KeyManagerFactory = KeyManagerFactory.getInstance("SunX509")
-  keyManagerFactory.init(ks, password)
-
-  val tmf: TrustManagerFactory = TrustManagerFactory.getInstance("SunX509")
-  tmf.init(ks)
-
-  val sslContext: SSLContext = SSLContext.getInstance("TLS")
-  sslContext.init(keyManagerFactory.getKeyManagers, tmf.getTrustManagers, new SecureRandom)
-  val https: HttpsConnectionContext = ConnectionContext.https(sslContext)
 
   private val interface = Try(args(0)).getOrElse("127.0.0.1")
 
@@ -59,7 +38,15 @@ object Main extends App {
       }
     }
 
-  private val shingledRoute = sqlShingle(logbackShingle(route))
+  private val shingledRoute = logbackShingle(route)
+
+  val sslConfig = AkkaSSLConfig.get(system)
+
+  val keyManagerFactory = sslConfig.buildKeyManagerFactory(sslConfig.config)
+  val trustManagerFactory = sslConfig.buildTrustManagerFactory(sslConfig.config)
+  val ctx = new ConfigSSLContextBuilder(new AkkaLoggerFactory(system), sslConfig.config, keyManagerFactory, trustManagerFactory).build()
+
+  val https: HttpsConnectionContext = ConnectionContext.https(ctx)
 
   Http().bindAndHandle(shingledRoute, interface, 8080)
   Http().bindAndHandle(shingledRoute, interface, 9443, https)
